47997dc9a6c83d82e11d52bd15c64092144fe9f7
Nikita <rotar.nick13@gmail.com>
2017-10-19 11:03:44 +0300

    header and general edits

--- app/js/modules/headerMouseOverMenu.js
+++ app/js/modules/headerMouseOverMenu.js
@@ -1,21 +1,21 @@
 +function() {
-    //header mouse over menu
-    var hoverLine = $('.hover_line');
-    //передаём координаты для line
-    var firstOffsetLeft = $('header .header_nav_wrapper li.active').offset().left;
-    //передаём ширину для line
-    var firstWidth = $('header .header_nav_wrapper li.active').outerWidth();
-    hoverLine.css({left: firstOffsetLeft + 'px', maxWidth: firstWidth + 'px'});
-    console.log(firstOffsetLeft);
 
+    window.addEventListener('load', function() {
+        //header mouse over menu
+        var hoverLine = $('.hover_line');
+        //передаём координаты для line
+        var firstOffsetLeft = $('header .header_nav_wrapper li.active').offset().left;
+        //передаём ширину для line
+        var firstWidth = $('header .header_nav_wrapper li.active').outerWidth();
+        hoverLine.css({left: firstOffsetLeft + 'px', maxWidth: firstWidth + 'px'});
 
-
-
-    $('header .header_nav_wrapper li').mouseover(function(){
-        var thisLi = $(this);
-        var offsetLeft = thisLi.position().left;
-        console.log(offsetLeft);
-        var width = thisLi.outerWidth();
-        hoverLine.css({left: offsetLeft + 'px', maxWidth: width + 'px'});
+        $('header .header_nav_wrapper li').mouseover(function(){
+            var thisLi = $(this);
+            var offsetLeft = thisLi.position().left;
+            console.log(firstOffsetLeft, offsetLeft);
+            var width = thisLi.outerWidth();
+            hoverLine.css({left: offsetLeft + 'px', maxWidth: width + 'px'});
+        });
     });
-}()
+
+}();
