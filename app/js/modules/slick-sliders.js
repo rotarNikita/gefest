d879abffea51fda1b95f29167b46800a8d15655b
Nikita <rotar.nick13@gmail.com>
2017-10-20 17:27:21 +0300

    home-project block and small general edits

--- app/js/modules/slick-sliders.js
+++ app/js/modules/slick-sliders.js
@@ -7,4 +7,20 @@
             adaptiveHeight: true
         })
     }
+
+    var homeProjectSlider = $('.home_project_slider');
+
+    if (homeProjectSlider.length) {
+        homeProjectSlider.slick({
+            adaptiveHeight: true
+        });
+
+        var homeProject = document.querySelector('.home_project');
+
+        homeProjectSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+            var imageSrc = document.querySelector('.home_project_slider [data-slick-index="' + nextSlide + '"]').dataset.back;
+
+            homeProject.style.backgroundImage = 'url(' + imageSrc + ')'
+        });
+    }
 }();
