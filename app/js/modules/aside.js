2e3b93bbbb43909297a7c0da4e8c122c0c67cb84
Nikita <rotar.nick13@gmail.com>
2017-11-16 02:28:31 +0200

    project page

---
+++ app/js/modules/aside.js
@@ -0,0 +1,183 @@
++function () {
+    // aside list
+    var aside = document.querySelector('.single-object_aside');
+
+    if (aside) {
+        var navItems = aside.querySelectorAll('.single-object_aside_li'),
+            navItemsActive = aside.querySelector('.single-object_aside_li.active'),
+            navLine = aside.querySelector('.line'),
+            navUl = aside.querySelector('.single-object_aside_ul'),
+            navUlHeight = navUl.offsetHeight;
+
+        navLine.style.top = navItemsActive.offsetTop + navItemsActive.offsetHeight / 2 + 'px';
+        navLine.style.bottom = navUlHeight - navLine.offsetTop + 'px';
+
+        window.addEventListener('load', function () {
+            navUlHeight = navUl.offsetHeight;
+            navLine.style.top = navItemsActive.offsetTop + navItemsActive.offsetHeight / 2 + 'px';
+            navLine.style.bottom = navUlHeight - navLine.offsetTop + 'px';
+        });
+
+        var scrollAnimationStop = function () {},
+            targets = [];
+
+        Array.prototype.forEach.call(navItems, function (navItem) {
+            // link in li
+            var link = navItem.children[1],
+                target = document.getElementById(link.attributes.href.value.slice(1));
+
+            targets.push(target);
+
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+            });
+
+            navItem.addEventListener('click', function () {
+                navItemsActive.classList.remove('active');
+                this.classList.add('active');
+                lineGoTo(this);
+
+                navItemsActive = this;
+
+                scrollAnimationStop();
+                window.removeEventListener('scroll', checkActiveLi);
+                var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop,
+                    targetOffset = N.offset(target).top;
+
+                scrollAnimationStop = N.animate({
+                    duration: 1000,
+                    timing: easeInOut,
+                    do: function (progress) {
+                        window.scrollTo(0, Math.round((currentScrollTop - currentScrollTop * progress) + targetOffset * progress));
+                    },
+                    callback: function () {
+                        window.addEventListener('scroll', checkActiveLi);
+                    }
+                })
+            });
+        });
+
+        window.addEventListener('scroll', checkActiveLi);
+
+        function checkActiveLi () {
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
+                offsets = [];
+
+            targets.forEach(function (target, i) {
+                offsets[i] = N.offset(target).top;
+            });
+
+            for (var i = 0; i < offsets.length; i++) {
+                if (offsets[i + 1]) {
+                    if ((scrollTop >= offsets[i] && scrollTop < offsets[i + 1]) || scrollTop <= offsets[0]) {
+                        setActiveLi(navItems[i]);
+                        break;
+                    }
+                } else setActiveLi(navItems[i])
+            }
+        }
+
+        function setActiveLi (navItem) {
+            navItemsActive.classList.remove('active');
+            navItem.classList.add('active');
+            lineGoTo(navItem);
+
+            navItemsActive = navItem;
+        }
+
+        var stopAnimationLine = function () {};
+
+        function lineGoTo (toNavItem) {
+            stopAnimationLine();
+
+            var top = parseInt(navLine.style.top),
+                bottom = parseInt(navLine.style.bottom),
+                newTop = toNavItem.offsetTop + toNavItem.offsetHeight / 2,
+                newBottom = navUlHeight - newTop,
+                deltaTop = newTop - top,
+                deltaBottom = newBottom - bottom;
+
+            if (deltaTop > deltaBottom) {
+                stopAnimationLine = N.animate({
+                    duration: 500,
+                    do: function (progress) {
+                        navLine.style.bottom = bottom + deltaBottom * progress + 'px'
+                    },
+                    timing: easeInOut,
+                    callback: function () {
+                        stopAnimationLine = N.animate({
+                            duration: 500,
+                            do: function (progress) {
+                                navLine.style.top = top + deltaTop * progress + 'px'
+                            },
+                            timing: easeInOut
+                        })
+                    }
+                })
+            } else {
+                stopAnimationLine = N.animate({
+                    duration: 500,
+                    do: function (progress) {
+                        navLine.style.top = top + deltaTop * progress + 'px'
+                    },
+                    timing: easeInOut,
+                    callback: function () {
+                        stopAnimationLine = N.animate({
+                            duration: 500,
+                            do: function (progress) {
+                                navLine.style.bottom = bottom + deltaBottom * progress + 'px'
+                            },
+                            timing: easeInOut
+                        })
+                    }
+                })
+            }
+        }
+
+        function easeInOut (t) {
+            return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+        }
+
+        // aside scroll
+        var maxTop, maxBottom, bottom,
+            header = document.querySelector('.header'),
+            footer = document.querySelector('.footer_after');
+
+        asideVariablesInit();
+        window.addEventListener('load', asideVariablesInit);
+
+        asideScroll();
+        window.addEventListener('scroll', asideScroll);
+
+        function asideVariablesCalc () {
+            maxTop = header.offsetHeight;
+            maxBottom = N.offset(footer).top;
+            bottom = footer.offsetHeight;
+        }
+
+        function asideVariablesInit () {
+            asideVariablesCalc();
+            asideScroll();
+        }
+
+        function asideScroll () {
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop,
+                scrollBottom = scrollTop + window.innerHeight;
+
+            asideVariablesCalc();
+
+            if (scrollTop > maxTop && scrollBottom < maxBottom) {
+                aside.className = 'single-object_aside';
+                aside.classList.add('fixed');
+            } else if (scrollTop <= maxTop) {
+                aside.className = 'single-object_aside';
+                aside.style.top = maxTop + 'px';
+                aside.classList.add('top');
+            } else {
+                aside.className = 'single-object_aside';
+                aside.style.bottom = bottom + 'px';
+                aside.classList.add('bottom');
+            }
+        }
+    }
+}();
---
+++ app/js/modules/project.js
@@ -0,0 +1,21 @@
++function () {
+    var projectPage = document.querySelector('.projects');
+
+    if (projectPage) {
+        // description hide
+        var sliders = projectPage.querySelectorAll('.projects_item');
+
+        Array.prototype.forEach.call(sliders, function (slider) {
+            var text = slider.querySelector('.projects_item_description'),
+                button = slider.querySelector('.single-object_slider_zoom');
+
+            button.addEventListener('click', function () {
+                text.classList.toggle('close');
+            });
+
+            text.addEventListener('click', function () {
+                this.classList.remove('close');
+            })
+        })
+    }
+}();
