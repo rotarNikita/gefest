b4cbc2269103619a64295dc36620daf0f4a590e3
Nikita <rotar.nick13@gmail.com>
2017-11-28 13:35:25 +0200

    select apartment range mobile events edit

--- app/js/modules/select-apartment.js
+++ app/js/modules/select-apartment.js
@@ -3,12 +3,14 @@
 
     if (range.length) {
         var from = $('.select-apartment_form_range .from'),
-            to = $('.select-apartment_form_range .to');
+            to = $('.select-apartment_form_range .to'),
+            min = +from[0].getAttribute('min'),
+            max = +from[0].getAttribute('max');
 
         range.slider({
             range: true,
-            min: 0,
-            max: 120,
+            min: min,
+            max: max,
             values: [ 17, 81 ],
             slide: function( event, ui ) {
                 from.val(ui.values[0]);
@@ -18,5 +20,39 @@
 
         from.val(range.slider("values", 0));
         to.val(range.slider("values", 1));
+
+        if (isTouchDevice()) {
+            from[0].removeAttribute('readonly');
+            to[0].removeAttribute('readonly');
+
+            from[0].setAttribute('type', 'number');
+            to[0].setAttribute('type', 'number');
+
+            from[0].addEventListener('change', function (event) {
+                if (+this.value > +to[0].value) this.value = to[0].value;
+                if (+this.value < min) this.value = min;
+
+                range.slider('values', 0, this.value)
+            });
+
+            to[0].addEventListener('change', function () {
+                if (+this.value < +from[0].value) this.value = from[0].value;
+                if (+this.value > max) this.value = max;
+
+                range.slider('values', 1, this.value)
+            })
+        }
+
+        function isTouchDevice () {
+            return 'ontouchstart' in window || navigator.maxTouchPoints;
+        }
+    }
+
+    var selectApartmentForm = document.querySelector('.select-apartment_form');
+
+    if (selectApartmentForm) {
+        selectApartmentForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+        })
     }
 }();
