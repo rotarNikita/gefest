3910bebb87eda41d937f0d3e8c32490504ec3b28
Nikita <rotar.nick13@gmail.com>
2018-03-06 16:26:22 +0200

    section choose edit for app

--- app/js/modules/section-choose.js
+++ app/js/modules/section-choose.js
@@ -1,21 +1,58 @@
 +function () {
     var flats = document.querySelectorAll('.section_choose_flat');
+    var svg = document.querySelectorAll('.section_choose');
+
+    if (svg.length) {
+        Array.prototype.forEach.call(svg, function (item) {
+            var image = item.querySelector('.section_choose_main-image');
+            var img = new Image();
+
+            img.addEventListener('load', function () {
+                image.setAttribute('width', img.naturalWidth + '');
+                image.setAttribute('height', img.naturalHeight + '');
+
+                item.setAttribute('viewBox', '0 0 ' + img.naturalWidth + ' ' + img.naturalHeight);
+            });
+
+            img.src = image.href.baseVal;
+        })
+    }
 
     if (flats.length) {
         var interval = 4000,
-            duration = 500;
+            duration = interval / flats.length;
 
         Array.prototype.forEach.call(flats, function (flat, flatIndex) {
+            var flatPath = flat.querySelector('.section_choose_path');
+            var fillColor = flatPath.getAttribute('fill');
+
             flat.addEventListener('click', function () {
                 window.location = this.dataset.href;
             });
 
-            setInterval(function () {
+            flat.addEventListener('mouseenter', hover);
+
+            flat.addEventListener('mouseleave', unHover);
+
+            function hover () {
                 flat.classList.add('hover');
-                setTimeout(function () {
-                    flat.classList.remove('hover');
-                }, duration)
-            }, interval + duration * flatIndex)
+                flatPath.style.fill = fillColor;
+            }
+
+            function unHover () {
+                flat.classList.remove('hover');
+                flatPath.style.fill = 'transparent';
+            }
+
+            setTimeout(function () {
+                setInterval(function () {
+                    hover();
+
+                    setTimeout(function () {
+                        unHover();
+                    }, duration);
+                }, interval)
+            }, duration * flatIndex);
         })
     }
 }();
