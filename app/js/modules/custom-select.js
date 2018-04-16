3ed07d47b4fc740efe644c67c397ea34677ae5f2
Nikita <rotar.nick13@gmail.com>
2018-04-16 13:24:53 +0300

    observer custom select

--- app/js/modules/custom-select.js
+++ app/js/modules/custom-select.js
@@ -10,6 +10,19 @@
                     options = dropdown.querySelectorAll('li'),
                     optionsHeight = 0;
 
+                var observer = new MutationObserver(function (mutations) {
+                    mutations.forEach(function (mutation) {
+                        mutation.addedNodes.forEach(function (node) {
+                            if (node.nodeName === 'LI') {
+                                node.removeEventListener('click', optionClick);
+                                node.addEventListener('click', optionClick)
+                            }
+                        })
+                    })
+                });
+
+                observer.observe(dropdown, {childList: true});
+
                 try {
                     var currentLi = dropdown.querySelector('li[data-value="' + dropdown.dataset.value + '"]');
 
@@ -21,19 +34,21 @@
                 Array.prototype.forEach.call(options, function (option) {
                     optionsHeight += option.offsetHeight;
 
-                    option.addEventListener('click', function () {
-                        var value = this.innerHTML;
-
-                        selectValue.innerHTML = value;
-                        selectInput.value = value;
-                        dropdown.dataset.value = this.dataset.value;
-
-                        var selectedOption = customSelectWrapper.querySelector('.section_select_dropdown li.active');
-
-                        if (selectedOption) selectedOption.classList.remove('active');
-                        this.classList.add('active');
-                    })
+                    option.addEventListener('click', optionClick)
                 });
+
+                function optionClick () {
+                    var value = this.innerHTML;
+
+                    selectValue.innerHTML = value;
+                    selectInput.value = value;
+                    dropdown.dataset.value = this.dataset.value;
+
+                    var selectedOption = customSelectWrapper.querySelector('.section_select_dropdown li.active');
+
+                    if (selectedOption) selectedOption.classList.remove('active');
+                    this.classList.add('active');
+                }
 
                 // max-height = 148px in style
                 if (optionsHeight > 148) customSelectWrapper.querySelector('.section_select_dropdown').style.overflowY = 'auto';
